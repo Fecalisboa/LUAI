968a07707a8a96968cd1e5b7ec2c5b68576ade0e
Fecalisboa <127762860+Fecalisboa@users.noreply.github.com>
2024-05-29 22:17:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,114 @@ import base64
 import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
-from backend import QnASystem
 from schema import TransformType, EmbeddingTypes, IndexerType, BotType
+import os
+from langchain import FAISS, OpenAI, HuggingFaceHub, Cohere, PromptTemplate
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings, CohereEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter, NLTKTextSplitter, SpacyTextSplitter
+from langchain.vectorstores import Chroma, ElasticVectorSearch
+from pypdf import PdfReader
+
+class QnASystem:
+
+    def read_and_load_pdf(self, f_data):
+        pdf_data = PdfReader(f_data)
+        documents = []
+        for idx, page in enumerate(pdf_data.pages):
+            documents.append(Document(page_content=page.extract_text(), metadata={"page_no": idx, "source": f_data.name}))
+        self.documents = documents
+
+    def document_transformer(self, transform_type: TransformType):
+        match transform_type:
+            case TransformType.CharacterTransform:
+                t_type = CharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+            case TransformType.RecursiveTransform:
+                t_type = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+            case TransformType.NLTKTransform:
+                t_type = NLTKTextSplitter()
+            case TransformType.SpacyTransform:
+                t_type = SpacyTextSplitter()
+            case _:
+                raise IndexError("Invalid Transformer Type")
+        self.transformed_documents = t_type.split_documents(documents=self.documents)
+
+    def generate_embeddings(self, embedding_type: EmbeddingTypes = EmbeddingTypes.OPENAI, indexer_type: IndexerType = IndexerType.FAISS, **kwargs):
+        temperature = kwargs.get("temperature", 0)
+        max_tokens = kwargs.get("max_tokens", 512)
+        match embedding_type:
+            case EmbeddingTypes.OPENAI:
+                os.environ["OPENAI_API_KEY"] = kwargs.get("api_key") or os.getenv("OPENAI_API_KEY")
+                embeddings = OpenAIEmbeddings()
+                llm = OpenAI(temperature=temperature, max_tokens=max_tokens)
+            case EmbeddingTypes.HUGGING_FACE:
+                embeddings = HuggingFaceEmbeddings(model_name=kwargs.get("model_name"))
+                llm = HuggingFaceHub(repo_id=kwargs.get("model_name"), model_kwargs={"temperature": temperature, "max_tokens": max_tokens})
+            case EmbeddingTypes.COHERE:
+                embeddings = CohereEmbeddings(model=kwargs.get("model_name"), cohere_api_key=kwargs.get("api_key"))
+                llm = Cohere(model=kwargs.get("model_name"), cohere_api_key=kwargs.get("api_key"), model_kwargs={"temperature": temperature, "max_tokens": max_tokens})
+            case _:
+                raise IndexError("Invalid Embedding Type")
+        match indexer_type:
+            case IndexerType.FAISS:
+                indexer = FAISS
+            case IndexerType.CHROMA:
+                indexer = Chroma()
+            case IndexerType.ELASTICSEARCH:
+                indexer = ElasticVectorSearch(elasticsearch_url=kwargs.get("elasticsearch_url"))
+            case _:
+                raise IndexError("Invalid Indexer Function")
+        self.llm = llm
+        self.indexer = indexer
+        self.vector_store = indexer.from_documents(documents=self.transformed_documents, embedding=embeddings)
+
+    def get_retriever(self, search_type="similarity", top_k=5, **kwargs):
+        retriever = self.vector_store.as_retriever(search_type=search_type, search_kwargs={"k": top_k})
+        self.retriever = retriever
+
+    def get_prompt(self, bot_type: BotType, **kwargs):
+        match bot_type:
+            case BotType.qna:
+                prompt = """
+                You are a smart and helpful AI assistant, who answers the question given context
+                {context}
+                Question: {question}
+                """
+            case BotType.conversational:
+                prompt = """
+                Given the following conversation and a follow-up question, 
+                rephrase the follow-up question to be a standalone question, in its original language.
+                \nChat History:\n{chat_history}\nFollow Up Input: {question}\nStandalone question:
+                """
+        return PromptTemplate(input_variables=["context", "question", "chat_history"], template=prompt)
+
+    def build_qa(self, qa_type: BotType, chain_type="stuff", return_documents: bool = True, **kwargs):
+        match qa_type:
+            case BotType.qna:
+                self.chain = RetrievalQA.from_chain_type(llm=self.llm, retriever=self.retriever, chain_type=chain_type, return_source_documents=return_documents, verbose=True)
+            case BotType.conversational:
+                self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key="answer")
+                self.chain = ConversationalRetrievalChain.from_llm(llm=self.llm, retriever=self.retriever, chain_type=chain_type, return_source_documents=return_documents, memory=self.memory, verbose=True)
+            case _:
+                raise IndexError("Invalid QA Type")
+
+    def ask_question(self, query):
+        if type(self.chain) == RetrievalQA:
+            data = {"query": query}
+        else:
+            data = {"question": query}
+        return self.chain(data)
+
+    def build_chain(self, transform_type, embedding_type, indexer_type, **kwargs):
+        if hasattr(self, "llm"):
+            return self.chain
+        self.document_transformer(transform_type)
+        self.generate_embeddings(embedding_type=embedding_type, indexer_type=indexer_type, **kwargs)
+        self.get_retriever(**kwargs)
+        qa = self.build_qa(qa_type=kwargs.get("bot_type"), **kwargs)
+        return qa
 
 kwargs = {}
 source_docs = []
@@ -15,8 +121,7 @@ if "qna_system" not in st.session_state:
 def show_pdf(f):
     f.seek(0)
     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="800" ' \
-                  f'type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="800" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 def model_settings():
@@ -38,22 +143,18 @@ with st.sidebar:
     text_transform = st.selectbox("Text Transformer", options=TransformType)
     st.markdown("---")
     selected_model = st.selectbox("Select Model", options=EmbeddingTypes)
+
+    # Inserir a chave da API diretamente no código
     match selected_model:
         case EmbeddingTypes.OPENAI:
-            api_key = st.text_input("OpenAI API Key", placeholder="sk-...", type="password")
-            if not api_key.startswith('sk-'):
-                st.warning('Please enter your OpenAI API key!', icon='⚠')
+            api_key = "sua_openai_api_key_aqui"
             model_settings()
         case EmbeddingTypes.HUGGING_FACE:
-            api_key = st.text_input("Hugging Face API Key", placeholder="hg-...", type="password")
-            if not api_key.startswith('hg-'):
-                st.warning('Please enter your HuggingFace API key!', icon='⚠')
-            kwargs["model_name"] = st.selectbox("Choose Model", options=["google/flan-t5-xxl"])
+            api_key = "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"
+            kwargs["model_name"] = st.selectbox("Choose Model", options=["mistralai/Mistral-7B-Instruct-v0.3"])
             model_settings()
         case EmbeddingTypes.COHERE:
-            api_key = st.text_input("Cohere API Key", placeholder="...", type="password")
-            if not api_key:
-                st.warning('Please enter your Cohere API key!', icon='⚠')
+            api_key = "OGY2ZCgZ4351TM0pXzRNeJLpw6o9GhyfWA3r05eW"
             model_settings()
         case _:
             api_key = None
@@ -84,8 +185,7 @@ with tab1:
             if selected_model == EmbeddingTypes.NA:
                 st.warning("Please select the model", icon='⚠')
             else:
-                st.session_state.qna_system.build_chain(transform_type=text_transform, embedding_type=selected_model,
-                                                        indexer_type=vector_indexer, **kwargs)
+                st.session_state.qna_system.build_chain(transform_type=text_transform, embedding_type=selected_model, indexer_type=vector_indexer, **kwargs)
 
 def generate_response(prompt):
     if prompt and uploaded_file:
@@ -148,8 +248,7 @@ with tab2:
                 with st.expander("Related Documents", expanded=False):
                     for source in source_docs:
                         metadata = source.metadata
-                        st.write("{source} - {page_no}".format(source=metadata.get("source"),
-                                                               page_no=metadata.get("page_no")))
+                        st.write("{source} - {page_no}".format(source=metadata.get("source"), page_no=metadata.get("page_no")))
                         st.write(source.page_content)
                         st.markdown("---")
 
@@ -160,3 +259,4 @@ with tab3:
         st.warning("Feature not enabled.", icon='⚠')
     else:
         st.warning("Please upload PDF", icon='⚠')
+
